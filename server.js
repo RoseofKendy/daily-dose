11eca725b55964c869a7792e86fa185c1f8a91b2
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-29 00:08:34 +0300

    Handles CORS

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins (or specify your frontend URL)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+    next();
+  });
+
 server.use(middlewares);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+  });
